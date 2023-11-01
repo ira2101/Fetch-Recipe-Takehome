b74063b81aa0b8e3f7e9fbdef20e3344979b1299
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-10-31 21:01:06 -0400

    Set up most of FTRecipeCardIngredientItemCheckmarkComponent. We need to create a symbol class so that we can add in the checkmark

--- Fetch-Recipe-Takehome/Controllers/Recipe Card/Ingredients/FTRecipeCardIngredientItemCheckmarkComponent.swift
+++ Fetch-Recipe-Takehome/Controllers/Recipe Card/Ingredients/FTRecipeCardIngredientItemCheckmarkComponent.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class FTRecipeCardIngredientItemCheckmarkComponent: FTToggleButton {
+    
+    private let unselectedBorderColor = UIColor(hex: 0xE9E9E9)
+    
+    private var container: FTHStack!
 
     override init() {
         super.init()
@@ -15,7 +19,39 @@ class FTRecipeCardIngredientItemCheckmarkComponent: FTToggleButton {
     }
     
     private func setupView() {
-        
+        self
+        .ftComponent(
+            Container()
+        )
+        .ftToggleButtonDidChangeState { [weak self] isActive in
+            guard let self = self else { return }
+            
+            self.toggleButtonDidChangeState(isActive)
+        }
+    }
+    
+    private func Container() -> UIView {
+        container =
+        FTHStack()
+        .ftWidth(28)
+        .ftHeight(28)
+        .ftBorder(width: 1, color: unselectedBorderColor)
+        .ftBackgroundColor(.clear)
+        return container
+    }
+    
+    private func toggleButtonDidChangeState(_ isActive: Bool) {
+        isActive ? onActivate() : onDeactivate()
+    }
+    
+    private func onActivate() {
+        container.ftBorder(width: 1, color: .clear)
+        container.ftBackgroundColor(UIColor(hex: 0xFCF3DB))
+    }
+    
+    private func onDeactivate() {
+        container.ftBorder(width: 1, color: unselectedBorderColor)
+        container.ftBackgroundColor(.clear)
     }
 
 }
