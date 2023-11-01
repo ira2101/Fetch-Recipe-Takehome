cbf719ffc438c8387aa94124a63112d6e0ae581b
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-11-01 16:48:02 -0400

    Created a networking class, FTNetworking

---
+++ Fetch-Recipe-Takehome/FTNetworking.swift
@@ -0,0 +1,39 @@
+//
+//  FTNetworking.swift
+//  Fetch-Recipe-Takehome
+//
+//  Created by Ira Einbinder on 11/1/23.
+//
+
+import Foundation
+import Alamofire
+import UIKit
+
+class FTNetworking {
+    
+    static let baseURL = "www.themealdb.com"
+    
+    static func readImage(uri: String, parameters: Parameters? = nil, completion: @escaping (Result<UIImage?, Error>) -> Void) {
+        AF.request(baseURL + uri, parameters: parameters).responseData { response in
+            switch response.result {
+            case .success(let data):
+                let image = UIImage(data: data)
+                completion(.success(image))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func readObject<T : Decodable>(uri: String, parameters: Parameters? = nil, completion: @escaping (Result<T, Error>) -> Void) {
+        AF.request(baseURL + uri, parameters: parameters).responseDecodable(of: T.self) { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+}
