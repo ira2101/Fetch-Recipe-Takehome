55c9c582a1c3aa921a08a85d17894fbb3862aa07
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-11-01 14:57:41 -0400

    Added the statistics to FTRecipeCardTitleComponents. Next step is to get rid of the other statistics component. I think the UI will look cleaner without it

--- Fetch-Recipe-Takehome/Controllers/Recipe Card/FTRecipeCardTitleComponent.swift
+++ Fetch-Recipe-Takehome/Controllers/Recipe Card/FTRecipeCardTitleComponent.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FTRecipeCardTitleComponent: FTHStack {
+class FTRecipeCardTitleComponent: FTVStack {
     
     override init() {
         super.init()
@@ -17,19 +17,82 @@ class FTRecipeCardTitleComponent: FTHStack {
     private func setupView() {
         self
         .ftAddArrangedSubview(
-            Label()
+            TitleLabel()
         )
+        .ftAddArrangedSubview(
+            guaranteedAlignment: .leading,
+            Stats()
+        )
+        .ftSpacing(8)
         .ftPaddingHorizontal(16)
         .ftPaddingVertical(24)
         .ftBackgroundColor(.systemBackground)
     }
     
-    private func Label() -> UIView {
+    private func TitleLabel() -> UIView {
         return FTLabel()
         .ftText("Title")
         .ftTextColor(FTColorPalette.labelPrimary)
         .ftFont(textStyle: .title2, weight: .semibold)
         .ftNumberOfLines(0)
+    }
+    
+    private func Stats() -> UIView {
+        return FTHStack()
+        .ftAddArrangedSubview(
+            IngredientsLabel()
+        )
+        .ftAddArrangedSubview(
+            FTLabel()
+            .ftText("\u{2022}")
+            .ftFont(textStyle: .caption2, weight: .regular)
+            .ftTextColor(FTColorPalette.labelSecondary)
+        )
+        .ftAddArrangedSubview(
+            StepsLabel()
+        )
+        .ftSpacing(12)
+        .ftAlignment(.center)
+    }
+    
+    private func IngredientsLabel() -> UIView {
+        return FTLabel()
+        .ftAttributedText(
+            AttributedString(count: 0, thing: "Ingredient")
+        )
+    }
+    
+    private func StepsLabel() -> UIView {
+        return FTLabel()
+        .ftAttributedText(
+            AttributedString(count: 0, thing: "Step")
+        )
+    }
+    
+    private func AttributedString(count: Int, thing: String) -> NSMutableAttributedString {
+        return FTAttributedStringBuilder()
+        .ftAppend(
+            FTAttributedString()
+            .ftText(count.formatted())
+            .ftTextColor(FTColorPalette.labelPrimary)
+            .ftFont(textStyle: .title3, weight: .bold)
+            .ftBuild()
+        )
+        .ftAppend(
+            FTAttributedString()
+            .ftText(" ")
+            .ftTextColor(FTColorPalette.labelPrimary)
+            .ftFont(textStyle: .title3, weight: .bold)
+            .ftBuild()
+        )
+        .ftAppend(
+            FTAttributedString()
+            .ftText(FTFormatter.pluralize(thing, count: count))
+            .ftTextColor(FTColorPalette.labelSecondary)
+            .ftFont(textStyle: .body, weight: .regular)
+            .ftBuild()
+        )
+        .ftBuild()
     }
 
 }
