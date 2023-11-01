9fe30d96dcce1d35f6d022dbb79d2cc8963fe162
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-10-31 21:21:04 -0400

    Set up the skeleton for steps - FTRecipeCardStepComponent, FTRecipeCardStepItemComponent

---
+++ Fetch-Recipe-Takehome/Controllers/Recipe Card/Steps/FTRecipeCardStepComponent.swift
@@ -0,0 +1,21 @@
+//
+//  FTRecipeCardStepComponent.swift
+//  Fetch-Recipe-Takehome
+//
+//  Created by Ira Einbinder on 10/31/23.
+//
+
+import UIKit
+
+class FTRecipeCardStepComponent: FTVStack {
+
+    override init() {
+        super.init()
+        setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+
+}
---
+++ Fetch-Recipe-Takehome/Controllers/Recipe Card/Steps/FTRecipeCardStepItemComponent.swift
@@ -0,0 +1,21 @@
+//
+//  FTRecipeCardStepItemComponent.swift
+//  Fetch-Recipe-Takehome
+//
+//  Created by Ira Einbinder on 10/31/23.
+//
+
+import UIKit
+
+class FTRecipeCardStepItemComponent: FTVStack {
+
+    override init() {
+        super.init()
+        setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+
+}
