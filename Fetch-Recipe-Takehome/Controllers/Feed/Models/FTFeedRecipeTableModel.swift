cfa77616963988a7bea39768c49355c215653884
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-11-01 20:00:16 -0400

    Added a function to read recipe overviews in FTFeedRecipeTableModel

--- Fetch-Recipe-Takehome/Controllers/Feed/Models/FTFeedRecipeTableModel.swift
+++ Fetch-Recipe-Takehome/Controllers/Feed/Models/FTFeedRecipeTableModel.swift
@@ -9,15 +9,15 @@ import Foundation
 
 class FTFeedRecipeTableModel {
     
-//    func readRecipes() {
-//        FTNetworking.readObject(type: FTCategory.self, uri: "/api/json/v1/1/filter.php?c=Seafood") { result in
-//            switch result {
-//            case .success(let success):
-//
-//            case .failure(let failure):
-//                <#code#>
-//            }
-//        }
-//    }
+    func readRecipes(completion: @escaping (Result<[FTRecipeOverview], Error>) -> Void) {
+        FTNetworking.readObject(type: FTRecipeOverviewResponseWrapper.self, uri: "/api/json/v1/1/filter.php?c=Dessert") { result in
+            switch result {
+            case .success(let recipeOverviewWrapper):
+                completion(.success(recipeOverviewWrapper.recipeOverviews))
+            case .failure(let failure):
+                completion(.failure(failure))
+            }
+        }
+    }
     
 }
