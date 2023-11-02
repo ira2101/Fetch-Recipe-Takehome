1edc3b7a14edfbf296b4b53b125e36146151e7aa
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-11-01 23:05:03 -0400

    Created FTFeedHeaderFilterBarDelegate

---
+++ Fetch-Recipe-Takehome/Controllers/Feed/Delegates/FTFeedHeaderFilterBarDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  FTFeedHeaderFilterBarDelegate.swift
+//  Fetch-Recipe-Takehome
+//
+//  Created by Ira Einbinder on 11/1/23.
+//
+
+import Foundation
+
+protocol FTFeedHeaderFilterBarDelegate: AnyObject {
+    
+    func ftFeedHeaderFilterBarDidChangeFilterTo(filter: String)
+    
+}
