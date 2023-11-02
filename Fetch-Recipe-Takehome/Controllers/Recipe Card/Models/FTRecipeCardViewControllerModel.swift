bc4960d1e7b5a5ddc2c0bf9ec003f3a73dc996e0
Ira Einbinder <54328228+ira2101@users.noreply.github.com>
2023-11-01 22:25:27 -0400

    Finished FTRecipeCardViewControllerModel

--- Fetch-Recipe-Takehome/Controllers/Recipe Card/Models/FTRecipeCardViewControllerModel.swift
+++ Fetch-Recipe-Takehome/Controllers/Recipe Card/Models/FTRecipeCardViewControllerModel.swift
@@ -9,4 +9,18 @@ import Foundation
 
 class FTRecipeCardViewControllerModel {
     
+    var recipe: FTRecipe
+    
+    init(recipe: FTRecipe) {
+        self.recipe = recipe
+    }
+    
+    func createBackgroundImageModel() -> FTRecipeCardBackgroundImageModel {
+        return FTRecipeCardBackgroundImageModel(thumbnailImageURL: recipe.thumbnailImageURL)
+    }
+    
+    func createVScrollModel() -> FTRecipeCardVScrollModel {
+        return FTRecipeCardVScrollModel(recipe: recipe)
+    }
+    
 }
